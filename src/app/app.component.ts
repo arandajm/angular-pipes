aa21ac008b1cf83ab56d752043e19ec56956bff4
Jesus Aranda <aranda.jm@icloud.com>
2020-04-10 01:22:08 -0300

    using date pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   PI: number = Math.PI;
   porcentaje: number = 0.234;
   salario: number = 1234.5;
+  fecha: Date = new Date();
   heroe: any = {
     nombre: 'Logan',
     clave: 'Wolverine',
